9aa520c95b0df240c16336ffee311a3eb4265b67
LorDTunE <37368884+LorDTunE@users.noreply.github.com>
2018-05-23 12:59:43 +0200

    Underlines

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 function mouseDown(){
     document.getElementById("but").style.color = "Green";
+    document.getElementById("but").textContent = "Nope.";
 }
 function mouseUp(){
     document.getElementById("but").style.color = "Red";
